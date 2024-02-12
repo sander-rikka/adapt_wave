62e2d6e044d06e844963c2c76ce192f1b5d968f6
sander.rikka <sander.rikka@gmail.com>
2024-02-12 23:33:05 +0200

    update before writing the loop for HS download and mean calculation.

--- read_opendap.py
+++ read_opendap.py
@@ -26,16 +26,34 @@ def copernicusmarine_datastore(dataset, username, password):
         data_store = xr.backends.PydapDataStore(open_url(url, session=session, user_charset='utf-8')) # needs PyDAP >= v3.3.0 see https://github.com/pydap/pydap/pull/223/commits
     return data_store
 
+# can the following code be used for the date generation for HS mean calculations.
 
+from datetime import date, timedelta
+"""
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+start_date = date(2013, 1, 1)
+end_date = date(2015, 6, 2)
+for single_date in daterange(start_date, end_date):
+    print(single_date.strftime("%Y-%m-%d"))
+"""
 
 if __name__ == '__main__':
-    USERNAME = getpass.getpass('Enter your username: ')
-    PASSWORD = getpass.getpass('Enter your password: ')
+    #pip install lxml
+    #USERNAME = getpass.getpass('Enter your username: ')
+    #PASSWORD = getpass.getpass('Enter your password: ')
     DATASET_ID = 'dataset-bal-reanalysis-wav-hourly'
     # DATASET_ID = 'cmems_mod_bal_wav_anfc_PT1h-i'
+    USERNAME = 'srikka2'
+    PASSWORD = 'ULtRAnYL2'
     data_store = copernicusmarine_datastore(DATASET_ID, USERNAME, PASSWORD)
 
     DS = xr.open_dataset(data_store)
-    # DS
+
+    time = pd.to_datetime(DS.time.data)
+    hs = DS.VHM0.sel(time=slice("2004-01-01", "2004-02-15")).mean('time').plot()
+    print(hs)
 
     print('The end!')
