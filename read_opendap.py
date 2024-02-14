e41eeadaefcaf64cd7758f113da93eeb89d1e56e
sander.rikka <sander.rikka@gmail.com>
2024-02-14 15:16:35 +0200

    main loop for downloading the data done!

--- read_opendap.py
+++ read_opendap.py
@@ -42,6 +42,7 @@ for single_date in daterange(start_date, end_date):
 
 if __name__ == '__main__':
     #pip install lxml
+    # from 2004 to end of 2013
     #USERNAME = getpass.getpass('Enter your username: ')
     #PASSWORD = getpass.getpass('Enter your password: ')
     DATASET_ID = 'dataset-bal-reanalysis-wav-hourly'
@@ -50,20 +51,36 @@ if __name__ == '__main__':
     PASSWORD = 'ULtRAnYL2'
     data_store = copernicusmarine_datastore(DATASET_ID, USERNAME, PASSWORD)
 
-    DS = xr.open_dataset(data_store)
+    labels_to_drop = ['VHM0_SW1', 'VHM0_SW2', 'VHM0_WW', 'VMDR_SW1', 'VMDR_SW2', 'VMDR_WW', 'VPED', 'VSDX', 'VSDY',
+                      'VTM01_SW1', 'VTM01_SW2', 'VTM01_WW', 'VTM10', 'VTPK']
+    DS = xr.open_dataset(data_store).drop_vars(labels_to_drop)
 
-    time = pd.to_datetime(DS.time.data)
-    hs_arr = []
-    for i in range(0, len(time), 15):
-        start_date = dt.datetime.strptime(time[i], '%Y-%m-%d')
-        end_date = dt.datetime.strptime(time[i + 15], '%Y-%m-%d') # siin peab mingi parem indekseerimine olema...
-        # praegu jookseb i+15 üle aastas olevate päevade arvu. aga vb ei peagi aasta kaupa jagama?
-        hs = DS.VHM0.sel(time=slice(f'{start_date}2004-01-01', f'{end_date}2004-02-15')).mean('time').plot()
-        if i == 0:
-            hs_arr = hs.reshape((1, 1, 256, 256))  # esimene tühi dimensioon pole vajalik
-        else:
-            hs_arr = np.append(hs_arr, hs.reshape((1, 1, 256, 256)),
-                                axis=0)
+    for year in np.arange(2004, 2014, 1):
+        for month in np.arange(1, 13, 1):
+            days_in_month = pd.Period(f'{year}-{month}').days_in_month
+            print(f'collecting data for year: {year}, month: {month} (days in month: {days_in_month})')
+            d1 = DS.sel(time=slice(f'{year}-{month}-{1}', f'{year}-{month}-{days_in_month//2}'))
+            d2 = DS.sel(time=slice(f'{year}-{month}-{days_in_month//2 + 1}', f'{year}-{month}-{days_in_month}'))
+            data_month = xr.concat([d1, d2], dim='time').to_netcdf(rf'E:\adapt_wave\{year}_{month}.nc', mode='w')
+            d1, d2 = None, None
+
+    # time = pd.to_datetime(DS.time.data)
+    # d1 = DS.sel(time=slice('2004-01-01 00:00:00', '2004-01-15 23:00:00'))
+    # d2 = DS.sel(time=slice('2004-01-16 00:00:00', '2004-01-31 23:00:00'))
+    # hs_jan = xr.concat([hs1, hs2], dim='time')
+    # data_jan = xr.concat([d1, d2], dim='time')
+    # data_jan.to_netcdf(path='2004_jan.nc', mode='w')
+    # hs_arr = []
+    # for i in range(0, len(time), 15):
+    #     start_date = dt.datetime.strptime(time[i], '%Y-%m-%d')
+    #     end_date = dt.datetime.strptime(time[i + 15], '%Y-%m-%d') # siin peab mingi parem indekseerimine olema...
+    #     # praegu jookseb i+15 üle aastas olevate päevade arvu. aga vb ei peagi aasta kaupa jagama?
+    #     hs = DS.VHM0.sel(time=slice(f'{start_date}2004-01-01', f'{end_date}2004-02-15')).mean('time').plot()
+    #     if i == 0:
+    #         hs_arr = hs.reshape((1, 1, 256, 256))  # esimene tühi dimensioon pole vajalik
+    #     else:
+    #         hs_arr = np.append(hs_arr, hs.reshape((1, 1, 256, 256)),
+    #                             axis=0)
         # kui aasta läbi, siis salvestada array? v salvestada kuude kaupa arrayd koos muutujatega, mis hiljem vajalikud oleks???
     #print(hs)
 
